4788eb2b24fe2c8d77f17a213681bd2e7e5eac3b
ShameemSheiky <shameem363636@gmail.com>
2025-08-09 19:46:50 +0530

    Unwanted Files

--- .htrmw2rnphfvtb.data/cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'qZ1EAOJBR5s9bXD1lB8d5Io7WF2SdWVUlms.circuitmakers.in';
-$CFG->bootstraphash = '41bacff3215782fc7323bafaa2e22816';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
--- auth/edwiserbridge/classes/core/output/progress_trace/null_progress_trace.php
+++
@@ -1,9 +0,0 @@
-<?php
-// File: auth/edwiserbridge/classes/core/output/progress_trace/null_progress_trace.php
-
-namespace core\output\progress_trace;
-
-class null_progress_trace {
-    public function output($message) {}
-    public function finished() {}
-} 
